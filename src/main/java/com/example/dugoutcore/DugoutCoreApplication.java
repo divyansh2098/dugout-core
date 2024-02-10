b224cdb1ead2a8f05a1ff3e21760d1383a8e2621
divyansh2098 <divyanshsingh2098@gmail.com>
2024-02-10 13:28:41 +0530

    Initial Commit

---
+++ src/main/java/com/example/dugoutcore/DugoutCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.dugoutcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DugoutCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DugoutCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dugoutcore/DugoutCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dugoutcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DugoutCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
